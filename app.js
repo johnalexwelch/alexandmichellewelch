c3f6767ebd82d3d197b2c1a3add2ce8cc4f2a0ef
Alex Welch <john.alexwelch@gmail.com>
2015-04-19 09:28:31 -0500

    Included new modules as well as bootstrap

--- app.js
+++ app.js
@@ -4,9 +4,12 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-
+var stylus = require('stylus');
+var mongo = require('mongoskin');
+var db = mongo.db("mongodb://localhost:27017/alexandmichelle", {native_parser:true});
 var routes = require('./routes/index');
 var users = require('./routes/users');
+
 
 var app = express();
 
@@ -21,6 +24,18 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(stylus.middleware({
+    src: __dirname + '/views',
+    dest: __dirname + '/public'
+}));
+
+
+
+// Make the database accessible to the router
+app.use(function(req,res,next){
+    req.db = db;
+    next();
+});
 
 app.use('/', routes);
 app.use('/users', users);
